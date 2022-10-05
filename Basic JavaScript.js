00f541405e59e127d2ca584b0cec6fd999053378
mnchkin <timurka.ignatev@mail.ru>
2022-10-05 19:09:54 +0300

    Task 7

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -22,3 +22,14 @@ var a = 9;
 //Task 6. Declare String Variables
 var myFirstName = 'Timur';
 var myLastName = 'Shirmanov';
+
+//Task 7. Understanding Uninitialized Variables
+var a;
+var b;
+var c;
+a = 5;
+b = 10;
+c = 'I am a';
+a = a + 1;
+b = b + 5;
+c = c + ' String!';
