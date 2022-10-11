fab5bf1e276a5142af81291c5d71c571a4d3a1be
mnchkin <timurka.ignatev@mail.ru>
2022-10-11 19:12:43 +0300

    Task 43

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -199,3 +199,12 @@ const myData = myArray[0];
 //Task 42. Modify Array Data With Indexes
 const myArray = [18, 64, 99];
 myArray[0] = 45;
+
+//Task 43. Access Multi-Dimensional Arrays With Indexes
+const myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+const myData = myArray[2][1];
