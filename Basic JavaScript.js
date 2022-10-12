07cce965028f21e705f148824f937e1f795be420
mnchkin <timurka.ignatev@mail.ru>
2022-10-12 19:21:11 +0300

    Task 50

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -247,3 +247,10 @@ function reusableFunction() {
   console.log('Hi World');
 }
 reusableFunction();
+
+//Task 50. Passing Values to Functions with Arguments
+function functionWithArgs(arg1, arg2) {
+  console.log(arg1 + arg2);
+}
+functionWithArgs(1, 2);
+functionWithArgs(7, 9);
