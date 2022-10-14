67548fca2d1cd4936a4a8f6f4e80950f56f36eed
mnchkin <timurka.ignatev@mail.ru>
2022-10-14 23:19:37 +0300

    Task 56

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -298,3 +298,12 @@ function addFive() {
 
 addThree();
 addFive();
+
+//Task 56. Assignment with a Returned Value
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+processed = processArg(7);
