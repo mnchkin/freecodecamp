94ea6504e7531607c5251768e171075db0933325
mnchkin <timurka.ignatev@mail.ru>
2022-10-14 23:28:34 +0300

    Task 57

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -307,3 +307,16 @@ function processArg(num) {
 }
 
 processed = processArg(7);
+
+//Task 57. Stand in Line
+function nextInLine(arr, item) {
+  var queue = arr.push(item);
+  var removeItem = arr.shift();
+  return removeItem;
+}
+
+const testArr = [1, 2, 3, 4, 5];
+
+console.log('Before: ' + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log('After: ' + JSON.stringify(testArr));
