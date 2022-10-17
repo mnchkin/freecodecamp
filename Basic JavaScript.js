5e43e486e6560f9b1848ddb9cfb10646bf93c564
mnchkin <timurka.ignatev@mail.ru>
2022-10-17 22:58:16 +0300

    Task 62

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -356,3 +356,14 @@ function testStrict(val) {
 }
 
 testStrict(10);
+
+//Task 62. Practice comparing different values
+function compareEquality(a, b) {
+  if (a === b) {
+    // Change this line
+    return 'Equal';
+  }
+  return 'Not Equal';
+}
+
+compareEquality(10, '10');
