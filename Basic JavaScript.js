b3c8e77c19b1572b66b4b13788abaadbbd06933a
mnchkin <timurka.ignatev@mail.ru>
2022-10-18 19:13:18 +0300

    Task 66

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -399,3 +399,18 @@ function testGreaterThan(val) {
 }
 
 testGreaterThan(10);
+
+//Task 66. Comparison with the Greater Than Or Equal To Operator
+function testGreaterOrEqual(val) {
+  if (val >= 20) {
+    return '20 or Over';
+  }
+
+  if (val >= 10) {
+    return '10 or Over';
+  }
+
+  return 'Less than 10';
+}
+
+testGreaterOrEqual(10);
