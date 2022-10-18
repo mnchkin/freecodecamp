3ba879393eea4ed224b3edce888b87fc080f2445
mnchkin <timurka.ignatev@mail.ru>
2022-10-18 19:29:04 +0300

    Task 71

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -464,3 +464,16 @@ function testLogicalOr(val) {
 }
 
 testLogicalOr(15);
+
+//Task 71. Introducing Else Statements
+function testElse(val) {
+  let result = '';
+  if (val > 5) {
+    result = 'Bigger than 5';
+  } else {
+    result = '5 or Smaller';
+  }
+  return result;
+}
+
+testElse(4);
