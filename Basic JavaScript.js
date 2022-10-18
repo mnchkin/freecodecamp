aef8d4ef8fc90b2c045056eee338f5bc640c91d4
mnchkin <timurka.ignatev@mail.ru>
2022-10-18 19:11:36 +0300

    Task 65

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -384,3 +384,18 @@ function testStrictNotEqual(val) {
 }
 
 testStrictNotEqual(10);
+
+//Task 65. Comparison with the Greater Than Operator
+function testGreaterThan(val) {
+  if (val > 100) {
+    return 'Over 100';
+  }
+
+  if (val > 10) {
+    return 'Over 10';
+  }
+
+  return '10 or Under';
+}
+
+testGreaterThan(10);
