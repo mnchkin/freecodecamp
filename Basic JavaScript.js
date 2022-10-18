735a94649d6149405eab0fcb0b161ca07f0874b3
mnchkin <timurka.ignatev@mail.ru>
2022-10-18 19:26:12 +0300

    Task 70

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -454,3 +454,13 @@ function testLogicalAnd(val) {
 }
 
 testLogicalAnd(10);
+
+//Task 70. Comparisons with the Logical Or Operator
+function testLogicalOr(val) {
+  if (val < 10 || val > 20) {
+    return 'Outside';
+  }
+  return 'Inside';
+}
+
+testLogicalOr(15);
