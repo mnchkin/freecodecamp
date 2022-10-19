74fe6fc610af522756488f6f58125143247b6166
mnchkin <timurka.ignatev@mail.ru>
2022-10-19 13:47:36 +0300

    Task 74

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -502,3 +502,21 @@ function orderMyLogic(val) {
 }
 
 orderMyLogic(7);
+
+//Task 74. Chaining If Else Statements
+function testSize(num) {
+  if (num < 5) {
+    return 'Tiny';
+  } else if (num < 10) {
+    return 'Small';
+  } else if (num < 15) {
+    return 'Medium';
+  } else if (num < 20) {
+    return 'Large';
+  } else if (num >= 20) {
+    return 'Huge';
+  } else {
+    return 'Change Me';
+  }
+}
+testSize(7);
