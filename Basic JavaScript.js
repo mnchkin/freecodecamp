44e64828a899c17768acaa0871077bac02ddbb49
mnchkin <timurka.ignatev@mail.ru>
2022-10-19 14:19:32 +0300

    Task 77

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -574,3 +574,25 @@ function caseInSwitch(val) {
 }
 
 caseInSwitch(1);
+
+//Task 77. Adding a Default Option in Switch Statements
+function switchOfStuff(val) {
+  let answer = '';
+  switch (val) {
+    case 'a':
+      answer = 'apple';
+      break;
+    case 'b':
+      answer = 'bird';
+      break;
+    case 'c':
+      answer = 'cat';
+      break;
+    default:
+      answer = 'stuff';
+      break;
+  }
+  return answer;
+}
+
+switchOfStuff(1);
