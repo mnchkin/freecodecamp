d930a4ac2b4c49ede3f969e06a34b8c40ddb1978
mnchkin <timurka.ignatev@mail.ru>
2022-10-19 14:25:36 +0300

    Task 78

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -596,3 +596,27 @@ function switchOfStuff(val) {
 }
 
 switchOfStuff(1);
+
+//Task 78. Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
+  let answer = '';
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = 'Low';
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = 'Mid';
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = 'High';
+  }
+  return answer;
+}
+
+sequentialSizes(1);
