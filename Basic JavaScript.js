f7c8aa4fabc8e97fec540b777516c1684f352c39
mnchkin <timurka.ignatev@mail.ru>
2022-10-19 14:30:26 +0300

    Task 79

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -620,3 +620,28 @@ function sequentialSizes(val) {
 }
 
 sequentialSizes(1);
+
+//Task 79. Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+  let answer = '';
+  switch (val) {
+    case 'bob':
+      answer = 'Marley';
+      break;
+    case 42:
+      answer = 'The Answer';
+      break;
+    case 1:
+      answer = 'There is no #1';
+      break;
+    case 99:
+      answer = 'Missed me by this much!';
+      break;
+    case 7:
+      answer = 'Ate Nine';
+      break;
+  }
+  return answer;
+}
+
+chainToSwitch(7);
