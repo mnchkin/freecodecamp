5e5f1056ff50140c0df0651b6c3629ccc7025faa
mnchkin <timurka.ignatev@mail.ru>
2022-10-19 14:09:21 +0300

    Task 76

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -552,3 +552,25 @@ function golfScore(par, strokes) {
   }
 }
 golfScore(5, 4);
+
+//Task 76. Selecting from Many Options with Switch Statements
+function caseInSwitch(val) {
+  let answer = '';
+  switch (val) {
+    case 1:
+      answer = 'alpha';
+      break;
+    case 2:
+      answer = 'beta';
+      break;
+    case 3:
+      answer = 'gamma';
+      break;
+    case 4:
+      answer = 'delta';
+      break;
+  }
+  return answer;
+}
+
+caseInSwitch(1);
