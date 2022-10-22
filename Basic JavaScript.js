69818977f1b9815b7180e35a8e5e311ec9c53f46
mnchkin <timurka.ignatev@mail.ru>
2022-10-22 19:38:45 +0300

    Task 90

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -775,3 +775,20 @@ const myDog = {
 };
 
 delete myDog.tails;
+
+//Task 90. Using Objects for Lookups
+function phoneticLookup(val) {
+  let result = '';
+  const lookup = {
+    alpha: 'Adams',
+    bravo: 'Boston',
+    charlie: 'Chicago',
+    delta: 'Denver',
+    echo: 'Easy',
+    foxtrot: 'Frank',
+  };
+  result = lookup[val];
+  return result;
+}
+
+phoneticLookup('charlie');
