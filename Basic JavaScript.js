ad09d0b8a901bf9b4b9cf1f22e027c0ffcd3994d
mnchkin <timurka.ignatev@mail.ru>
2022-10-22 19:23:20 +0300

    Task 88

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -754,3 +754,13 @@ const myDog = {
 };
 
 myDog.name = 'Happy Coder';
+
+//Task 88. Add New Properties to a JavaScript Object
+const myDog = {
+  name: 'Happy Coder',
+  legs: 4,
+  tails: 1,
+  friends: ['freeCodeCamp Campers'],
+};
+
+myDog.bark = 'woof';
