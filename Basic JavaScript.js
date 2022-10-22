34d29e1e01b237570be2efa1bdc527b70002a0dc
mnchkin <timurka.ignatev@mail.ru>
2022-10-22 19:24:45 +0300

    Task 89

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -764,3 +764,14 @@ const myDog = {
 };
 
 myDog.bark = 'woof';
+
+//Task 89. Delete Properties from a JavaScript Object
+const myDog = {
+  name: 'Happy Coder',
+  legs: 4,
+  tails: 1,
+  friends: ['freeCodeCamp Campers'],
+  bark: 'woof',
+};
+
+delete myDog.tails;
