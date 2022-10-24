29a3208b4121a7e677b93baba8c46471df01d76c
mnchkin <timurka.ignatev@mail.ru>
2022-10-24 14:11:34 +0300

    Task 98

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -903,3 +903,10 @@ const myArray = [];
 for (let i = 1; i <= 5; i++) {
   myArray.push(i);
 }
+
+//Task 98. Iterate Odd Numbers With a For Loop
+const myArray = [];
+
+for (let i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
