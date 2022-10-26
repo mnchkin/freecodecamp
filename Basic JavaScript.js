a5abac66e2191e90efd648803d2eeeb9067a70d5
mnchkin <timurka.ignatev@mail.ru>
2022-10-26 22:05:11 +0300

    Task 110

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -1030,3 +1030,10 @@ function convertToInteger(str) {
 }
 
 convertToInteger('10011');
+
+//Task 110. Use the Conditional (Ternary) Operator
+function checkEqual(a, b) {
+  return a == b ? 'Equal' : 'Not Equal';
+}
+
+checkEqual(1, 2);
