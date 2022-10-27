27dacb287559221dccbf5d6b4a2f73fc24597bb5
mnchkin <timurka.ignatev@mail.ru>
2022-10-27 19:00:21 +0300

    Task 112

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -1044,3 +1044,15 @@ function checkSign(num) {
 }
 
 checkSign(10);
+
+//Task 112. Use Recursion to Create a Countdown
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countdown(n - 1);
+    countArray.unshift(n);
+    return countArray;
+  }
+}
+console.log(countdown(5));
