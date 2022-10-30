367e8295451eba121f3f478e90b841bd20e93949
mnchkin <timurka.ignatev@mail.ru>
2022-10-30 15:18:12 +0300

    Task 5

--- ES6.js
+++ ES6.js
@@ -35,3 +35,7 @@ const PI = freezeObj();
 
 //Task 4. Use Arrow Functions to Write Concise Anonymous Functions
 const magic = () => new Date();
+
+//Task 5. Write Arrow Functions with Parameters
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+console.log(myConcat([1, 2], [3, 4, 5]));
