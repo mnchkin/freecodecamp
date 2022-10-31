dca5212b8212725ea13591df780dbd249808d285
mnchkin <timurka.ignatev@mail.ru>
2022-10-31 16:16:37 +0300

    Task 7

--- ES6.js
+++ ES6.js
@@ -42,3 +42,8 @@ console.log(myConcat([1, 2], [3, 4, 5]));
 
 //Task 6. Set Default Parameters for Your Functions
 const increment = (number, value = 1) => number + value;
+
+//Task 7. Use the Rest Parameter with Function Parameters
+const sum = (...args) => {
+  return args.reduce((a, b) => a + b, 0);
+};
