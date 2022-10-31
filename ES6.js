af12d78cd3458a9075a3c2a48be5ca3fedf67319
mnchkin <timurka.ignatev@mail.ru>
2022-10-31 16:05:27 +0300

    Task 6

--- ES6.js
+++ ES6.js
@@ -39,3 +39,6 @@ const magic = () => new Date();
 //Task 5. Write Arrow Functions with Parameters
 const myConcat = (arr1, arr2) => arr1.concat(arr2);
 console.log(myConcat([1, 2], [3, 4, 5]));
+
+//Task 6. Set Default Parameters for Your Functions
+const increment = (number, value = 1) => number + value;
