b32b197d7a36d7d5af126737a18b154784b293f4
mnchkin <timurka.ignatev@mail.ru>
2022-11-01 10:02:26 +0300

    Task 17

--- ES6.js
+++ ES6.js
@@ -133,3 +133,13 @@ const failuresList = makeList(result.failure);
 
 //Task 16. Write Concise Object Literal Declarations Using Object Property Shorthand
 const createPerson = (name, age, gender) => ({ name, age, gender });
+
+//Task 17. Write Concise Declarative Functions with ES6
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  },
+};
+bicycle.setGear(3);
+console.log(bicycle.gear);
