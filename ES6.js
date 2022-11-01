e87d49fde7b3c99a856d5bc511c802327e2f3a2e
mnchkin <timurka.ignatev@mail.ru>
2022-11-01 15:12:38 +0300

    Task 21

--- ES6.js
+++ ES6.js
@@ -180,3 +180,12 @@ temp = thermos.temperature; // 26 in Celsius
     <script type="module" src="index.js"></script>
   </body>
 </html>;
+
+//Task 21. Use export to Share a Code Block
+export const uppercaseString = (string) => {
+  return string.toUpperCase();
+};
+
+export const lowercaseString = (string) => {
+  return string.toLowerCase();
+};
