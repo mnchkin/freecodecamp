f7de86b3244a632974b03cec2e02fddc25154156
mnchkin <timurka.ignatev@mail.ru>
2022-11-01 15:38:25 +0300

    Task 28

--- ES6.js
+++ ES6.js
@@ -224,3 +224,19 @@ const makeServerRequest = new Promise((resolve, reject) => {
     reject('Data not received');
   }
 });
+
+//Task 28. Handle a Fulfilled Promise with then
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+
+  if (responseFromServer) {
+    resolve('We got the data');
+  } else {
+    reject('Data not received');
+  }
+});
+
+makeServerRequest.then((result) => {
+  console.log(result);
+});
