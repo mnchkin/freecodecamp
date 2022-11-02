9e774750b3059a1b9f24ad47969bb261ed0e7e7b
mnchkin <timurka.ignatev@mail.ru>
2022-11-02 17:54:02 +0300

    Task 5

--- Regular Expressions.js
+++ Regular Expressions.js
@@ -19,3 +19,8 @@ let result = petRegex.test(petString);
 let myString = 'freeCodeCamp';
 let fccRegex = /freecodecamp/i;
 let result = fccRegex.test(myString);
+
+//Task 5. Extract Matches
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/;
+let result = extractStr.match(codingRegex);
