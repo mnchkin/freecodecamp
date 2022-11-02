f5db6ff017f4623438d1be3cb946c316f37e4d05
mnchkin <timurka.ignatev@mail.ru>
2022-11-02 18:14:26 +0300

    Task 9

--- Regular Expressions.js
+++ Regular Expressions.js
@@ -43,3 +43,9 @@ let quoteSample =
   'Beware of bugs in the above code; I have only proved it correct, not tried it.';
 let vowelRegex = /[aeiou]/gi;
 let result = quoteSample.match(vowelRegex);
+
+//Task 9. Match Letters of the Alphabet
+//Извлечение всех букв
+let quoteSample = 'The quick brown fox jumps over the lazy dog.';
+let alphabetRegex = /[a-z]/gi;
+let result = quoteSample.match(alphabetRegex);
