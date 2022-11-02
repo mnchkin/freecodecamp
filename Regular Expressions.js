5a4b5e7a2b84207247731a59782ccc248bb4e355
mnchkin <timurka.ignatev@mail.ru>
2022-11-02 12:42:18 +0300

    Task 2

--- Regular Expressions.js
+++ Regular Expressions.js
@@ -3,3 +3,8 @@
 let myString = 'Hello, World!';
 let myRegex = /Hello/;
 let result = myRegex.test(myString);
+
+//Task 2. Match Literal Strings
+let waldoIsHiding = 'Somewhere Waldo is hiding in this text.';
+let waldoRegex = /Waldo/;
+let result = waldoRegex.test(waldoIsHiding);
