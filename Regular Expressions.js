536cdf558d465e7ca73c0d7eb600c6a5d0a35bae
mnchkin <timurka.ignatev@mail.ru>
2022-11-03 18:51:15 +0300

    Task 19

--- Regular Expressions.js
+++ Regular Expressions.js
@@ -96,3 +96,9 @@ let result = lastRegex.test(caboose);
 let quoteSample = 'The five boxing wizards jump quickly.';
 let alphabetRegexV2 = /\w/g;
 let result = quoteSample.match(alphabetRegexV2).length;
+
+//Task 19. Match Everything But Letters and Numbers
+//Извлекает всё кроме букв и цифр
+let quoteSample = 'The five boxing wizards jump quickly.';
+let nonAlphabetRegex = /\W/g;
+let result = quoteSample.match(nonAlphabetRegex).length;
