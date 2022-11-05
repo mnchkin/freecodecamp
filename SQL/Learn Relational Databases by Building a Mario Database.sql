867000fae02231b10450a01b4ec785384cced2b8
mnchkin <timurka.ignatev@mail.ru>
2022-11-05 20:14:29 +0300

    Task 6-10

--- SQL/Learn Relational Databases by Building a Mario Database.sql
+++ SQL/Learn Relational Databases by Building a Mario Database.sql
@@ -11,4 +11,19 @@ psql --username=freecodecamp --dbname=postgres
 CREATE DATABASE first_database;
 
 --Task 5
-/l
+\l
+
+--Task 6
+CREATE DATABASE second_database;
+
+--Task 7
+\l
+
+--Task 8. Подключение БД
+\c second_database
+
+--Task 9. Показать таблицы
+\d 
+
+--Task 10. Создание таблицы
+CREATE TABLE first_table();
