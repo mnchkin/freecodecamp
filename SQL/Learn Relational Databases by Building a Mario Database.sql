bb4eba413df427124c25b932dcc082c3e8793eaa
mnchkin <timurka.ignatev@mail.ru>
2022-11-05 20:08:43 +0300

    Task 1-5

---
+++ SQL/Learn Relational Databases by Building a Mario Database.sql
@@ -0,0 +1,14 @@
+--Task 1
+echo hello PostgreSQL
+
+--Task 2
+psql --username=freecodecamp --dbname=postgres
+
+--Task 3. Список
+\l
+
+--Task 4. Создание БД
+CREATE DATABASE first_database;
+
+--Task 5
+/l
