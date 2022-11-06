2a59097934a0723f337640947007d0f936abe4eb
mnchkin <timurka.ignatev@mail.ru>
2022-11-06 18:17:15 +0300

    Task @

--- JS/Debugging.js
+++ JS/Debugging.js
@@ -5,3 +5,9 @@ a++;
 
 let sumAB = a + b;
 console.log(a);
+
+//Task 2. Understanding the Differences between the freeCodeCamp and Browser Console
+let output =
+  'Get this to show once in the freeCodeCamp console and not at all in the browser console';
+console.log(output);
+console.clear();
