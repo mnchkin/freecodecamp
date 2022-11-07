b7efc70cad1dd718dd2eea3222f295af37c53074
mnchkin <timurka.ignatev@mail.ru>
2022-11-07 21:29:12 +0300

    Task 5

--- JS/Debugging.js
+++ JS/Debugging.js
@@ -24,3 +24,8 @@ let receivables = 10;
 let payables = 8;
 let netWorkingCapital = receivables - payables;
 console.log(`Net working capital is: ${netWorkingCapital}`);
+
+//Task 5. Catch Unclosed Parentheses, Brackets, Braces and Quotes
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) => previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
