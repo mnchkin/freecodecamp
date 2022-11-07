12658e4185355f7c43ad5b20a401808729d511f3
mnchkin <timurka.ignatev@mail.ru>
2022-11-07 21:36:07 +0300

    Task 8

--- JS/Debugging.js
+++ JS/Debugging.js
@@ -46,3 +46,13 @@ if (x == y) {
 }
 
 console.log(result);
+
+//Task 8. Catch Missing Open and Closing Parenthesis After a Function Call
+function getNine() {
+  let x = 6;
+  let y = 3;
+  return x + y;
+}
+
+let result = getNine();
+console.log(result);
