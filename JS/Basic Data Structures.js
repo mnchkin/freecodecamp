ef6c72e971274a64f929b4abe66882807cee6ecc
mnchkin <timurka.ignatev@mail.ru>
2022-11-08 16:19:18 +0300

    Task 2

--- JS/Basic Data Structures.js
+++ JS/Basic Data Structures.js
@@ -1,2 +1,7 @@
 //Task 1. Use an Array to Store a Collection of Data
 let yourArray = ['cat', 7, true, 'dog', 5];
+
+//Task 2. Access an Array's Contents Using Bracket Notation
+let myArray = ['a', 'b', 'c', 'd'];
+myArray[1] = 'f';
+console.log(myArray);
