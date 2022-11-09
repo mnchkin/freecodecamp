fb5331c6bf0bf04960c47d124e8422f903e0875e
mnchkin <timurka.ignatev@mail.ru>
2022-11-09 18:22:51 +0300

    Task 3

--- JS/Basic Data Structures.js
+++ JS/Basic Data Structures.js
@@ -5,3 +5,13 @@ let yourArray = ['cat', 7, true, 'dog', 5];
 let myArray = ['a', 'b', 'c', 'd'];
 myArray[1] = 'f';
 console.log(myArray);
+
+//Task 3. Add Items to an Array with push() and unshift()
+//push() - добавляет элемент в конец массива; unshift() - в начало
+function mixedNumbers(arr) {
+  arr.unshift('I', 2, 'three');
+  arr.push(7, 'VIII', 9);
+  return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
