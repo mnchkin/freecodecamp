d9488d878cdf0460ea0edd424ba8c39dbbd45630
mnchkin <timurka.ignatev@mail.ru>
2022-11-10 16:26:15 +0300

    Task 8

--- JS/Basic Data Structures.js
+++ JS/Basic Data Structures.js
@@ -57,3 +57,16 @@ function forecast(arr) {
 console.log(
   forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms'])
 );
+
+//Task 8. Copy an Array with the Spread Operator
+//Копирование массива
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    newArr.push([...arr]);
+    num--;
+  }
+  return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
