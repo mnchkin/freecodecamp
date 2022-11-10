e428353c123f3557a3ceacb1df95393fd44de776
mnchkin <timurka.ignatev@mail.ru>
2022-11-10 19:22:18 +0300

    Task 11

--- JS/Basic Data Structures.js
+++ JS/Basic Data Structures.js
@@ -90,3 +90,27 @@ function quickCheck(arr, elem) {
 }
 
 console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+//Task 11. Iterate Through All an Array's Items Using For Loops
+//Если есть такое значение, удаляет вложенный массив с таким значением
+function filteredArray(arr, elem) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].indexOf(elem) == -1) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+console.log(
+  filteredArray(
+    [
+      [3, 2, 3],
+      [1, 6, 3],
+      [3, 13, 26],
+      [19, 3, 9],
+    ],
+    3
+  )
+);
