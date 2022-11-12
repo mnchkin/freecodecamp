473884178ed0c2c5ad47da93509b7e8a3f300763
mnchkin <timurka.ignatev@mail.ru>
2022-11-12 21:00:46 +0300

    Task 19

--- JS/Basic Data Structures.js
+++ JS/Basic Data Structures.js
@@ -249,3 +249,30 @@ function countOnline(usersObj) {
 }
 
 console.log(countOnline(users));
+
+//Task 19. Generate an Array of All Object Keys with Object.keys()
+//Генерирует массив из объектов ключа
+let users = {
+  Alan: {
+    age: 27,
+    online: false,
+  },
+  Jeff: {
+    age: 32,
+    online: true,
+  },
+  Sarah: {
+    age: 48,
+    online: false,
+  },
+  Ryan: {
+    age: 19,
+    online: true,
+  },
+};
+
+function getArrayOfUsers(obj) {
+  return Object.keys(obj);
+}
+
+console.log(getArrayOfUsers(users));
