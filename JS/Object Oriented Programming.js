960fc77282cd57e9e6da962f449e1c32abcbb822
mnchkin <timurka.ignatev@mail.ru>
2022-11-14 17:11:43 +0300

    Task 3

--- JS/Object Oriented Programming.js
+++ JS/Object Oriented Programming.js
@@ -10,3 +10,14 @@ let dog = {
   numLegs: 4,
 };
 console.log(dog.name, dog.numLegs);
+
+//Task 3. Create a Method on an Object
+let dog = {
+  name: 'Spot',
+  numLegs: 4,
+  sayLegs: function () {
+    return 'This dog has ' + dog.numLegs + ' legs.';
+  },
+};
+
+dog.sayLegs();
