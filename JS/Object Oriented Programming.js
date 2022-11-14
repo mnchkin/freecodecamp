4b3280759e8adf031c157e8c33b13bde21d086cb
mnchkin <timurka.ignatev@mail.ru>
2022-11-14 17:28:33 +0300

    Task 8:

--- JS/Object Oriented Programming.js
+++ JS/Object Oriented Programming.js
@@ -56,3 +56,11 @@ function Dog(name, color) {
 }
 
 let terrier = new Dog('Albus', 'brown');
+
+//Task 8. Verify an Object's Constructor with instanceof
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+
+let myHouse = new House(1);
+myHouse instanceof House;
