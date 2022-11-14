4ecd2925a155719f0c4aeb3475fd396d7fbe4afc
mnchkin <timurka.ignatev@mail.ru>
2022-11-14 17:32:56 +0300

    Task 9

--- JS/Object Oriented Programming.js
+++ JS/Object Oriented Programming.js
@@ -64,3 +64,21 @@ function House(numBedrooms) {
 
 let myHouse = new House(1);
 myHouse instanceof House;
+
+//Task 9. Understand Own Properties
+//Добавляет все собственные свойства canary в массив ownProps
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+
+let canary = new Bird('Tweety');
+let ownProps = [];
+
+for (let property in canary) {
+  if (canary.hasOwnProperty(property)) {
+    ownProps.push(property);
+  }
+}
+
+console.log(ownProps);
