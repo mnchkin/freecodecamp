af82c40cdf9d328fa91c92043011e20e7904ec9c
mnchkin <timurka.ignatev@mail.ru>
2022-11-14 12:10:55 +0300

    Task 4

--- JS/Basic Algorithm Scripting.js
+++ JS/Basic Algorithm Scripting.js
@@ -26,3 +26,19 @@ function factorialize(num) {
 }
 
 factorialize(5);
+
+//Task 4. Find the Longest Word in a String
+function findLongestWordLength(str) {
+  let words = str.split(' ');
+  let maxLength = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+    }
+  }
+
+  return maxLength;
+}
+
+findLongestWordLength('The quick brown fox jumped over the lazy dog');
