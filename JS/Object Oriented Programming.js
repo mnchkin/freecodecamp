edbaf90b41e0e1fa9913cf6a3f0f394724773beb
mnchkin <timurka.ignatev@mail.ru>
2022-11-14 17:13:45 +0300

    Task 4

--- JS/Object Oriented Programming.js
+++ JS/Object Oriented Programming.js
@@ -21,3 +21,14 @@ let dog = {
 };
 
 dog.sayLegs();
+
+//Task 4. Make Code More Reusable with the this Keyword
+let dog = {
+  name: 'Spot',
+  numLegs: 4,
+  sayLegs: function () {
+    return 'This dog has ' + this.numLegs + ' legs.';
+  },
+};
+
+dog.sayLegs();
