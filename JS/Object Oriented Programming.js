52b1a96f019de8102827d205ca510aa5fdd03ced
mnchkin <timurka.ignatev@mail.ru>
2022-11-14 17:08:42 +0300

    Task 2

--- JS/Object Oriented Programming.js
+++ JS/Object Oriented Programming.js
@@ -3,3 +3,10 @@ let dog = {
   name: 'Khan',
   numLegs: 4,
 };
+
+//Task 2. Use Dot Notation to Access the Properties of an Object
+let dog = {
+  name: 'Spot',
+  numLegs: 4,
+};
+console.log(dog.name, dog.numLegs);
