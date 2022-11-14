9fb610031ba81b9f5c7fe6a22d883822be01bd27
mnchkin <timurka.ignatev@mail.ru>
2022-11-14 16:44:24 +0300

    Task 14

--- JS/Basic Algorithm Scripting.js
+++ JS/Basic Algorithm Scripting.js
@@ -160,3 +160,17 @@ function bouncer(arr) {
 function bouncer(arr) {
     return arr.filter(Boolean);
   }*/
+
+//Task 14. Where do I Belong
+//Под каким индексом стоит
+function getIndexToIns(arr, num) {
+  arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= num) return i;
+  }
+
+  return arr.length;
+}
+
+getIndexToIns([40, 60], 50);
