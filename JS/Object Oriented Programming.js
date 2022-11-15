7dc21c1936ef581bca1122b74f2ee425c3f6627a
mnchkin <timurka.ignatev@mail.ru>
2022-11-15 16:08:01 +0300

    Task 14

--- JS/Object Oriented Programming.js
+++ JS/Object Oriented Programming.js
@@ -122,3 +122,18 @@ function joinDogFraternity(candidate) {
   }
   return false;
 }
+
+//Task 13. Change the Prototype to a New Object
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = {
+  numLegs: 4,
+  eat: function () {
+    console.log('nom nom nom');
+  },
+  describe: function () {
+    console.log('My name is ' + this.name);
+  },
+};
