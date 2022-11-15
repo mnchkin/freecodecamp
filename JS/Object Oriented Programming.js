37cbc9edc4d2477c11d80a87755e781e4a1991f3
mnchkin <timurka.ignatev@mail.ru>
2022-11-15 16:23:18 +0300

    Task 17

--- JS/Object Oriented Programming.js
+++ JS/Object Oriented Programming.js
@@ -173,3 +173,29 @@ let beagle = new Dog('Snoopy');
 Dog.prototype.isPrototypeOf(beagle);
 
 Object.prototype.isPrototypeOf(Dog.prototype);
+
+//Task 17. Use Inheritance So You Don't Repeat Yourself
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat,
+};
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear,
+};
+
+function Animal() {}
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function () {
+    console.log('nom nom nom');
+  },
+};
