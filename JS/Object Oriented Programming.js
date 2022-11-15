33c0e6c812742645a77f6276dac2137b9d4b7290
mnchkin <timurka.ignatev@mail.ru>
2022-11-15 16:37:55 +0300

    Task 20

--- JS/Object Oriented Programming.js
+++ JS/Object Oriented Programming.js
@@ -227,3 +227,21 @@ Animal.prototype = {
 
 let duck = Object.create(Animal.prototype);
 let beagle = Object.create(Animal.prototype);
+
+//Task 20. Reset an Inherited Constructor Property
+//Сброс унаследованного свойства конструктора
+function Animal() {}
+function Bird() {}
+function Dog() {}
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+let duck = new Bird();
+let beagle = new Dog();
+
+Bird.prototype.constructor = Bird;
+duck.constructor;
+
+Dog.prototype.constructor = Dog;
+beagle.constructor;
