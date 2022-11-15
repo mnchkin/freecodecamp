4e025190da9c7bc4df8ea3edeaab8c4556e8b0b8
mnchkin <timurka.ignatev@mail.ru>
2022-11-15 16:50:43 +0300

    Task 22

--- JS/Object Oriented Programming.js
+++ JS/Object Oriented Programming.js
@@ -262,3 +262,22 @@ Dog.prototype.bark = function () {
 };
 
 let beagle = new Dog();
+
+//Task 22. Override Inherited Methods
+//Переопределение унаследованных методов
+function Bird() {}
+
+Bird.prototype.fly = function () {
+  return 'I am flying!';
+};
+
+function Penguin() {}
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+Penguin.prototype.fly = function () {
+  return 'Alas, this is a flightless bird.';
+};
+
+let penguin = new Penguin();
+console.log(penguin.fly());
