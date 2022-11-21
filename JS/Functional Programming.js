6f07d6bb9e9b9b41e274e57e6d66b5ea6f03c367
mnchkin <112181723+mnchkin@users.noreply.github.com>
2022-11-21 14:15:24 +0300

    Task 6

--- JS/Functional Programming.js
+++ JS/Functional Programming.js
@@ -116,3 +116,22 @@ let result = arg + 1;
 return result;
 
 }
+
+//Task 6. Refactor Global Variables Out of Functions
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+function add(arr, bookName) {
+  let newArr = [...arr]; // Copy the bookList array to a new array.
+  newArr.push(bookName); // Add bookName parameter to the end of the new array.
+  return newArr;
+}
+
+function remove(arr, bookName) {
+  let newArr = [...arr]; // Copy the bookList array to a new array.
+  if (newArr.indexOf(bookName) >= 0) {
+    // Check whether the bookName parameter is in new array.
+    newArr.splice(newArr.indexOf(bookName), 1); // Remove the given paramater from the new array.
+    return newArr;
+    }
+}
