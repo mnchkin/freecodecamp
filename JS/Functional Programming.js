4c0cefb4ed5a2fea2c0f254f09ed38194560d7bf
mnchkin <112181723+mnchkin@users.noreply.github.com>
2022-11-21 14:38:19 +0300

    Task 8

--- JS/Functional Programming.js
+++ JS/Functional Programming.js
@@ -266,3 +266,16 @@ const users = [
 
 const names = users.map(user => user.name);
 console.log(names);
+
+//Task 8. Implement map on a Prototype
+Array.prototype.myMap = function(callback) {
+  const newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i]));
+  }
+  return newArray;
+};
+
+// Test case
+const s = [23, 65, 98, 5];
+const doubled_s = s.myMap(item => item * 2);
