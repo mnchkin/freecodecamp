9bdfacddc649b1c8477b2a707c351343e390b381
mnchkin <timurka.ignatev@mail.ru>
2022-11-24 20:05:26 +0300

    Task 18

--- JS/Functional Programming.js
+++ JS/Functional Programming.js
@@ -631,3 +631,14 @@ function alphabeticalOrder(arr) {
 }
 
 alphabeticalOrder(['a', 'd', 'c', 'a', 'z', 'g']);
+
+//Task 18. Return a Sorted Array Without Changing the Original Array
+const globalArray = [5, 6, 3, 2, 9];
+
+function nonMutatingSort(arr) {
+  return [].concat(arr).sort(function (a, b) {
+    return a - b;
+  });
+}
+
+nonMutatingSort(globalArray);
