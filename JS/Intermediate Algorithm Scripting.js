76e9f6b9f90a2e2ffcca8f30a7347c9fbf46084a
mnchkin <timurka.ignatev@mail.ru>
2023-04-27 21:40:16 +0300

    Task 17

--- JS/Intermediate Algorithm Scripting.js
+++ JS/Intermediate Algorithm Scripting.js
@@ -264,3 +264,17 @@ function steamrollArray(arr) {
 }
 
 steamrollArray([1, [2], [3, [[4]]]]); // возвращает [1, 2, 3, 4].
+
+//Task 17. Binary Agents
+function binaryAgent(str) {
+  var biString = str.split(' ');
+  var uniString = [];
+  for (var i = 0; i < biString.length; i++) {
+    uniString.push(String.fromCharCode(parseInt(biString[i], 2)));
+  }
+  return uniString.join('');
+}
+
+binaryAgent(
+  '01001001 00100000 01101100 01101111 01110110 01100101 00100000 01000110 01110010 01100101 01100101 01000011 01101111 01100100 01100101 01000011 01100001 01101101 01110000 00100001'
+); // возвращает I love FreeCodeCamp!
