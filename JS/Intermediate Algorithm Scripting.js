edbc8102f1faa7ae99d168481aa72429f324e57c
mnchkin <timurka.ignatev@mail.ru>
2023-04-27 21:27:22 +0300

    Task 14

--- JS/Intermediate Algorithm Scripting.js
+++ JS/Intermediate Algorithm Scripting.js
@@ -216,3 +216,31 @@ function sumPrimes(num) {
 }
 
 sumPrimes(977); // возвращает 73156.
+
+//Task 14. Smallest Common Multiple. Наименьшее общее кратное
+function smallestCommons(arr) {
+  // Setup
+  const [min, max] = arr.sort((a, b) => a - b);
+  const numberDivisors = max - min + 1;
+  // Largest possible value for SCM
+  let upperBound = 1;
+  for (let i = min; i <= max; i++) {
+    upperBound *= i;
+  }
+  // Test all multiples of 'max'
+  for (let multiple = max; multiple <= upperBound; multiple += max) {
+    // Check if every value in range divides 'multiple'
+    let divisorCount = 0;
+    for (let i = min; i <= max; i++) {
+      // Count divisors
+      if (multiple % i === 0) {
+        divisorCount += 1;
+      }
+    }
+    if (divisorCount === numberDivisors) {
+      return multiple;
+    }
+  }
+}
+
+smallestCommons([5, 1]); // возвращает 60.
