25b274badfdd422ddb6e8997a7610362b815758b
mnchkin <timurka.ignatev@mail.ru>
2023-04-30 22:58:29 +0300

    Task 20

--- JS/Intermediate Algorithm Scripting.js
+++ JS/Intermediate Algorithm Scripting.js
@@ -304,3 +304,35 @@ function addTogether() {
 
 addTogether(23, 30); // возвращает 53.
 addTogether(2, '3'); // возвращает undefined.
+
+//Task 20. Make a Person
+const Person = function (firstAndLast) {
+  let fullName = firstAndLast;
+
+  this.getFirstName = function () {
+    return fullName.split(' ')[0];
+  };
+
+  this.getLastName = function () {
+    return fullName.split(' ')[1];
+  };
+
+  this.getFullName = function () {
+    return fullName;
+  };
+
+  this.setFirstName = function (name) {
+    fullName = name + ' ' + fullName.split(' ')[1];
+  };
+
+  this.setLastName = function (name) {
+    fullName = fullName.split(' ')[0] + ' ' + name;
+  };
+
+  this.setFullName = function (name) {
+    fullName = name;
+  };
+};
+
+const bob = new Person('Bob Ross');
+bob.getFullName(); // возвращает Bob Ross.
