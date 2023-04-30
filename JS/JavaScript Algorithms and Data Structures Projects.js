87d44e62323d22f8b6851bf3bf207d4d32d6dcb5
mnchkin <timurka.ignatev@mail.ru>
2023-04-30 23:22:15 +0300

    Task 2

--- JS/JavaScript Algorithms and Data Structures Projects.js
+++ JS/JavaScript Algorithms and Data Structures Projects.js
@@ -7,3 +7,38 @@ function palindrome(str) {
 }
 
 palindrome('A man, a plan, a canal. Panama'); //возвращает true.
+
+//Task 2. Roman Numeral Converter. Конвертер римских цифр
+function convertToRoman(num) {
+  const numeralsObj = {
+    1000: 'M',
+    900: 'CM',
+    500: 'D',
+    400: 'CD',
+    100: 'C',
+    90: 'XC',
+    50: 'L',
+    40: 'XL',
+    10: 'X',
+    9: 'IX',
+    5: 'V',
+    4: 'IV',
+    1: 'I',
+  };
+  const getClosestNumeral = (n) =>
+    Object.keys(numeralsObj)
+      .filter((e) => e <= n)
+      .slice(-1)[0];
+
+  let numPlaceholder = num;
+  let romanNumeral = '';
+
+  while (numPlaceholder >= 1) {
+    let closestNumeral = getClosestNumeral(numPlaceholder);
+    romanNumeral += numeralsObj[closestNumeral];
+    numPlaceholder -= closestNumeral;
+  }
+  return romanNumeral;
+}
+
+convertToRoman(83); //возвращает LXXXIII
